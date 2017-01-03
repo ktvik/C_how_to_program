b39c7783de9cb23b600842d332a804b368023cbf
Kai Tore Vik <kaitorevik@kaitore.local>
2017-01-03 21:29:45 +0100

    Initial Commit

---
+++ Oppgave 3.41/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Oppgave 3.41
+//
+//  Created by Kai Tore Vik on 03/01/2017.
+//  Copyright © 2017 Kai Tore Vik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+ // insert code here...
+ printf("Hello, World!\n");
+    return 0;
+}
