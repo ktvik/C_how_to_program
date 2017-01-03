59ee75d445038daf9e0764f4b22461a4cf46afc7
Kai Tore Vik <kaitorevik@kaitore.local>
2017-01-03 22:23:31 +0100

    Initial commit

--- Oppgave 3.23/main.c
+++ Oppgave 3.23/main.c
@@ -5,11 +5,40 @@
 //  Created by Kai Tore Vik on 03/01/2017.
 //  Copyright © 2017 Kai Tore Vik. All rights reserved.
 //
+/*
+ Pseudo code
+ 
+ Ask the user to input 10 numbers, one after the other
+ Test if the new nubmer is the highest number
+ 
+ */
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
- // insert code here...
- printf("Hello, World!\n");
-    return 0;
-}
+int main(void)
+{
+ int counter=0, number, largest=0; // variable declarations
+ 
+ while(++counter <= 10)
+  {
+  printf("%s%d%s", "Enter a positive number (", counter, "/10): ");
+  scanf("%d", &number);
+  
+   if (number > largest )
+    {
+    largest = number;
+    }
+  else
+   {
+   continue;
+   }
+  } // End of while loop
+ 
+ puts("");
+ printf("%s%d\n", "The largest number is: ",largest);
+ 
+ puts("Press any key to exit...");
+ scanf("%*C");
+ 
+ return 0;
+} // End of main
