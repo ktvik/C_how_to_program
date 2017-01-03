5cf56ca469ec2e8b3f06864f48036842a3b2e689
Kai Tore Vik <kaitorevik@kaitore.local>
2017-01-03 22:23:31 +0100

    Initial commit

--- Oppgave 3.41/main.c
+++ Oppgave 3.41/main.c
@@ -5,11 +5,31 @@
 //  Created by Kai Tore Vik on 03/01/2017.
 //  Copyright © 2017 Kai Tore Vik. All rights reserved.
 //
+/*
+ Pseudo code
+ 
+ Ask user for radius of a circle
+ Print out diameter, circumference and area of the circle
+ 
+*/
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
- // insert code here...
- printf("Hello, World!\n");
-    return 0;
+int main(void)
+{
+ float pi=3.14159, radius, diameter, circumference, area;
+ 
+ printf("%s", "Enter radius of circle: ");
+ scanf("%f", &radius);
+ 
+ // Calculate
+ diameter = radius * 2;
+ circumference = 2 * pi * radius;
+ area = pi * radius * radius;
+ 
+ puts("dia.\tcirc.\tarea");
+ printf("%.3f\t%.3f\t%.3f\n", diameter, circumference, area);
+ 
+ 
+ return 0;
 }
