4a1c7ed4d84837c7105d2d81367e5c2236a7dfee
Kai Tore Vik <kaitorevik@kaitore.local>
2017-01-03 22:23:31 +0100

    Initial commit

--- Oppgave 3.25/main.c
+++ Oppgave 3.25/main.c
@@ -5,11 +5,32 @@
 //  Created by Kai Tore Vik on 03/01/2017.
 //  Copyright © 2017 Kai Tore Vik. All rights reserved.
 //
+/*
+ Pseudo code
+ 
+ Print a table for values of A, A+3, A+6 and A+9 with A=7 to 35
+*/
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
- // insert code here...
- printf("Hello, World!\n");
-    return 0;
-}
+int main(void)
+{
+ int A=7;
+ 
+ // Print heading
+ printf("%s\t%s\t%s\t%s\n","A","A+3","A+6","A+9");
+ puts("");
+ 
+ while (A <= 35)
+  {
+  printf("%d\t%d\t%d\t%d\n",A,A+3,A+6,A*9);
+  
+  A += 7; // increase A
+  } // End of loop
+ 
+ puts("");
+ puts("Press enter to exit...");
+ scanf("%*C");
+ 
+ return 0;
+} // End of main
